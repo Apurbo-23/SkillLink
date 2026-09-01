27e16d04d67d62c9cde24ad2cce648dbacc45b3a
Apurbo-23 <saiful.islam5@g.bracu.ac.bd>
2026-09-01 12:04:13 +0600

    removed category

--- skillink/resources/views/layouts/navigation.blade.php
+++ skillink/resources/views/layouts/navigation.blade.php
@@ -71,9 +71,7 @@
                     <x-nav-link :href="route('availability.index')" :active="request()->routeIs('availability.*')">
                         {{ __('Availability') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
-                        {{ __('Categories') }}
-                    </x-nav-link>
+                    
                 </div>
             </div>
 
@@ -191,8 +189,6 @@
         <x-responsive-nav-link :href="route('availability.index')" :active="request()->routeIs('availability.*')">
             {{ __('Availability') }}
         </x-responsive-nav-link>
-        <x-responsive-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
-            {{ __('Categories') }}
-        </x-responsive-nav-link>
+        
     </div>
 </nav>
